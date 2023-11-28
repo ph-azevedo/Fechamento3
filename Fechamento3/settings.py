ccf4acd721eff875ba2ea88ef85acf8851fa9017
Pedro <pedrohazevedo@gmail.com>
2023-11-28 20:52:51 -0300

    funcionando

--- Fechamento3/settings.py
+++ Fechamento3/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-oc_c2fcr)fcm8*8d4#%u_ets#gpl%5y3p0!#ulx2#vdeb&-p6#'
+SECRET_KEY = 'django-insecure-oc_c2fcr)fcm8*8d4#%u_ets#gpl%5y3p0!#ulx2#vdeb&-p6#'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+#SECRET_KEY = os.environ.get("SECRET_KEY")
+#DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
